a1999c245eb3fc50bdd8dc9b8d81402a7a43f4ff
vilander1337 <117035823+vilander1337@users.noreply.github.com>
2023-03-30 01:17:56 +0400

    create windows install script
    powershell pbmodular install script

---
+++ install.ps1
@@ -0,0 +1,41 @@
+Clear-Host
+
+Write-Output "
+        _/_/_/    _/_/_/    _/      _/                  _/            _/                      
+       _/    _/  _/    _/  _/_/  _/_/    _/_/      _/_/_/  _/    _/  _/    _/_/_/  _/  _/_/   
+      _/_/_/    _/_/_/    _/  _/  _/  _/    _/  _/    _/  _/    _/  _/  _/    _/  _/_/        
+     _/        _/    _/  _/      _/  _/    _/  _/    _/  _/    _/  _/  _/    _/  _/           
+    _/        _/_/_/    _/      _/    _/_/      _/_/_/    _/_/_/  _/    _/_/_/  _/            
+"
+
+git clone https://github.com/PBModular/bot PBModular
+
+cd PBModular
+
+pip install -r requirements.txt
+
+Copy-Item config.example.yaml config.yaml
+
+Clear-Host
+
+Write-Output "
+        _/_/_/    _/_/_/    _/      _/                  _/            _/                      
+       _/    _/  _/    _/  _/_/  _/_/    _/_/      _/_/_/  _/    _/  _/    _/_/_/  _/  _/_/   
+      _/_/_/    _/_/_/    _/  _/  _/  _/    _/  _/    _/  _/    _/  _/  _/    _/  _/_/        
+     _/        _/    _/  _/      _/  _/    _/  _/    _/  _/    _/  _/  _/    _/  _/           
+    _/        _/_/_/    _/      _/    _/_/      _/_/_/    _/_/_/  _/    _/_/_/  _/            
+"
+
+$bottoken = Read-Host "Enter bot token"
+$api_id = Read-Host "Enter api id"
+$api_hash = Read-Host "Enter api hash"
+$username = Read-Host "Enter telegram username/id"
+
+(Get-Content -Path "config.yaml") -replace 'token: null', ('token: ' + $bottoken) | Set-Content -Path "config.yaml"
+(Get-Content -Path "config.yaml") -replace 'api-id: null', ('api-id: ' + $api_id) | Set-Content -Path "config.yaml"
+(Get-Content -Path "config.yaml") -replace 'api-hash: null', ('api-hash: ' + $api_hash) | Set-Content -Path "config.yaml"
+(Get-Content -Path "config.yaml") -replace 'owner: "sanyapilot"', ('owner: ' + $username) | Set-Content -Path "config.yaml"
+
+Clear-Host
+
+python main.py 
